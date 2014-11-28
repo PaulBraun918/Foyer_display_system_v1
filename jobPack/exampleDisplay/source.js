52cd329b14e0b8f72019eadff7e44e6d7a0cb06f
PaulBraun918 <paul.braun918@gmail.com>
2014-11-28 13:59:12 +0100

    Foyer Display System v1.0

---
+++ jobPack/exampleDisplay/source.js
@@ -0,0 +1,256 @@
+var page_id=0;
+
+function getcurrenttime()
+{
+	var d = new Date();
+	var year=d.getYear()+1900;
+	var month=d.getMonth();
+	var date=d.getDate();
+	var hour=d.getHours();
+	var minute=d.getMinutes();
+	var sec= d.getSeconds();
+	var current_time=year+"-"+month+"-"+date+" "+ hour+":"+minute+":"+sec;
+	
+	return current_time;
+
+}
+
+function compare_time(current_time,start_time)
+{
+
+	var is_show=false;
+	var n = current_time.localeCompare(start_time);
+	if(n == -1)
+		is_show = false;
+	else
+		is_show = true;
+
+	return is_show;
+}
+function is_div_show(dev_index)
+{
+
+}
+
+function event_time_hour_min_sec(str)
+{
+	var result=str.split(" ");
+	return result[1];
+}
+function return_event_only_start_time(events,etf)
+{
+	if(etf=="yyyy:mm:dd hh:mm:ss")
+		return events["eventStart"];
+	else
+		return event_time_hour_min_sec(events["eventStart"]);
+}
+function return_event_start_and_end_time(events,etf,ietS)
+{
+	if(etf=="yyyy:mm:dd hh:mm:ss")
+		return events["eventStart"]+ietS+events["eventEnd"];
+	else
+		return event_time_hour_min_sec(events["eventStart"])+ietS+event_time_hour_min_sec(events["eventEnd"]);
+}
+
+function show_page(epp,tpp,toe,turl,evd,fade,fadeTime,iet,ietS,etf,events,page_id)
+{
+	getcurrenttime();
+
+	var events_per_page=epp;
+	var time_for_page=tpp;
+	var event_add_or_remove=evd;
+	var trigger_on_end=toe;
+	var events_count=(events.length);
+
+	var text;
+
+	for(i=page_id*events_per_page;i<page_id*events_per_page+min(events_per_page,events_count-page_id*events_per_page);i++)
+	{
+
+		var eventLineOne=document.createElement("div");
+		eventLineOne.setAttribute("class","eventLineOne");
+		var text = document.createTextNode(events[i]["title1"]);
+		eventLineOne.appendChild(text); 
+
+
+		var eventLineTwo=document.createElement("div");
+		eventLineTwo.setAttribute("class","eventLineTwo");
+		text = document.createTextNode(events[i]["title2"]);
+		eventLineTwo.appendChild(text); 
+
+
+		var eventTime=document.createElement("div");
+		eventTime.setAttribute("class","eventTime");
+		if(iet==true)
+		{
+			text = document.createTextNode(return_event_start_and_end_time(events[i],etf,ietS));
+		}
+		else
+		{
+			text = document.createTextNode(return_event_only_start_time(events[i],etf));
+		}
+		eventTime.appendChild(text); 
+
+
+
+		var eventRoom=document.createElement("div");
+		eventRoom.setAttribute("class","eventRoom");
+		var room_name;
+		if(events[i]["roomName"]=="")
+		{
+			var result=events[i]["roomNames"].split(",");
+			room_name=result[0];
+		}
+		else
+		{
+			room_name=events[i]["roomName"];
+		}
+		text = document.createTextNode(room_name);
+		eventRoom.appendChild(text); 
+
+
+		var eventArrow=document.createElement("div");
+		eventArrow.setAttribute("class","eventArrow");
+		var image=document.createElement("div");
+		image.setAttribute("class","arrow");
+		var image_inside=document.createElement("div");
+		image_inside.setAttribute("class","arrow");
+		image.appendChild(image_inside);
+
+		var fileName;
+		if(events[i]["alternateDirections"]==1)
+		{
+			fileName="arrows/"+events[i]["arrowNameAlt"]+".png";
+		}
+		else
+		{
+			fileName="arrows/"+events[i]["arrowName"]+".png";
+		}
+
+		image.setAttribute("style","background-image:url("+fileName+")");
+		image_inside.setAttribute("style","background-image:url("+fileName+")");
+		eventArrow.appendChild(image);
+
+
+		var eventText=document.createElement("div");
+		eventText.setAttribute("class","eventText");
+		var txt;
+		if(events[i]["alternateDirections"]==1)
+			txt=events[i]["textAlt"];
+		else
+			txt=events[i]["text"];
+		text = document.createTextNode(txt);
+		eventText.appendChild(text);
+
+
+		var eventLogo=document.createElement("div");
+		eventLogo.setAttribute("class","eventLogo");
+		
+		if(events[i]["logoHide"]!=1)
+		{
+			var logo=document.createElement("div");
+			var logo_inside = document.createElement("div");
+			logo.appendChild(logo_inside)
+
+			images = "img/";
+			var logoJpgFile=images + events[i]["id"]+".jpg";
+			var logoPNGFile=images + events[i]["id"]+".png";
+			
+			logo.setAttribute("class","eventLogoImg ");
+			logo.setAttribute("style","background-image:url(" + logoJpgFile + ")");
+			
+			logo_inside.setAttribute("class","eventLogoImg ps0");
+			logo_inside.setAttribute("style","background-image:url(" + logoPNGFile + ")");
+			
+			
+			eventLogo.appendChild(logo);
+			
+		}
+
+
+		var container=document.createElement("div");
+		container.setAttribute("class","evtCont container");
+		container.appendChild(eventLineOne);
+		container.appendChild(eventLineTwo);
+		container.appendChild(eventTime);
+		container.appendChild(eventRoom);
+		container.appendChild(eventArrow);
+		container.appendChild(eventText);
+		container.appendChild(eventLogo);
+
+		if(evd =="evtCont")
+		{
+			var row=document.createElement("div");
+			row.setAttribute("class", "row"+"-"+page_id);
+			row.appendChild(container);
+
+
+			document.body.appendChild(row);
+		}
+		else
+		{
+			var rowAlt=document.createElement("div");
+			rowAlt.setAttribute("class", "rowAlt"+"-"+page_id);
+			rowAlt.appendChild(container);
+
+
+			document.body.appendChild(rowAlt);
+		}
+	}
+
+	setTimeout(function(){hide_page(page_id,evd);},1000*tpp);
+}
+
+function show(epp,tpp,toe,turl,evd,fade,fadeTime,iet,ietS,etf,events)
+{
+	var events_per_page=epp;
+	var events_count=(events.length);
+	var page_count=float2int(events_count/(events_per_page))+1;
+	show_page(epp,tpp,toe,turl,evd,fade,fadeTime,iet,ietS,etf,events,page_id);
+
+	page_id++;
+	var timer_id=setInterval(function(){show_page(epp,tpp,toe,turl,evd,fade,fadeTime,iet,ietS,etf,events,page_id);
+		if(page_id<page_count-1)
+		{
+			page_id++;	
+		}
+		else
+		{
+			if(toe==true)
+			{
+				clearInterval(timer_id);
+				setTimeout(function(){window.location.href = turl;},1000*tpp);
+			}
+			else
+			{
+				page_id=0;
+			}
+		}
+
+	},1000*tpp);
+	
+}
+
+function min(var1,var2)
+{
+	return (var1>=var2)? var2:var1;
+}
+
+function float2int (value) {
+    return value | 0;
+}
+
+function hide_page(page_id,evd)
+{
+	
+	if(evd == "evtCont")
+	{
+		var className=".row-"+page_id;
+		$(className).remove();
+	}
+	else
+	{
+		var className=".rowAlt-"+page_id;
+		$(className).remove();
+	}
+}
